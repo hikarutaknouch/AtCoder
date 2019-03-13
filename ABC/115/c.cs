131f64cea9f395be38978b955d0e41fad65c40fb
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-03-13 15:52:06 +0900

    115-c

--- ABC/115/c.cs
+++ ABC/115/c.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class Program
@@ -7,29 +8,14 @@ class Program
         int[] s = Console.ReadLine().Split().Select(int.Parse).ToArray();
         int n = s[0];
         int k = s[1];
-        int[] h = new int[k];
-        for (int i = 0; i < n; i ++) {
-            int tmp = int.Parse(Console.ReadLine());
-            if (i < k) h[i] = tmp;
-            else {
-                if (h[k-1] - h[0] == 0) break;
-                Array.Sort(h);
-                int max = tmp - h[k-1];
-                int min = h[0] - tmp;
-                if (tmp - h[k-1] <= 0 && h[0] - tmp <= 0) {
-                    if (max >= min) 
-                    {
-                        if (h[k-1]-h[0] > h[k-1]-tmp) h[0] = tmp;
-                    }
-                    else if (max < min) {
-                        if (h[k-1]-h[0] > tmp-h[0]) h[k-1] = tmp;
-                    }
-                    else continue;
-                }
-                else continue;
-            }
+        List<int> h = new List<int>();
+        for (int i = 0; i < n; i ++) h.Add(int.Parse(Console.ReadLine()));
+        h.Sort();
+        int ans = h[k-1]-h[0];
+        for (int i = 1; i < n-k+1; i ++) {
+            int tmp = h[i+k-1] - h[i];
+            if(tmp<ans) ans = tmp;
         }
-        Array.Sort(h);
-        Console.WriteLine(h[k-1] - h[0]);
+        Console.WriteLine(ans);
     }
 }
