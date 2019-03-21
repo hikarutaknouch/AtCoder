a96bd0b09f803f815362e7bfc41b5cda7dbe8a47
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-03-21 21:25:47 +0900

    abc081 cがやっと解けた

--- ABC/081/c.cs
+++ ABC/081/c.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class Program
@@ -11,16 +10,8 @@ class Program
     }
     static int solve(int n, int k, int[] a) {
         int[] list = new int[n];
-        for (int i = 0; i < n; i ++) list[i] = 0;
         for (int i = 0; i < n; i ++) list[a[i]-1] ++;
         Array.Sort(list);
-        int count = 0;
-        while(true) {
-            if (list.Count(x => x != 0) <= k) break;
-            int index = n - list.Count(x => x == 0) - 1;
-            list[index] --;
-            count ++;
-        }
-        return count;
+        return list.Where((x,y) => y < n-k).Sum();
     }
 }
