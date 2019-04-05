dcce3814199e647ba419b93c3b27ef209dbaf2f0
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-04-05 15:24:39 +0900

    abc110

--- ABC/114/c.cs
+++ ABC/114/c.cs
@@ -2,17 +2,11 @@ using System;
 
 class Program
 {
-    public static int[] s = new int[3, 5, 7];
-    public static int count = 0;
-    public static long n = long.Parse(Console.ReadLine());
-    public static void Main(string[] args) {
-        Console.WriteLine(dfs(0,0));
+    static long n = long.Parse(Console.ReadLine());
+    static void Main(string[] args) {
+        Console.WriteLine(solve(n, n.Length));
     }
-    public static void dfs() {
-        long[][] array = new long[n.Length][];
-        for (int i = 3; i <= n.Length; i ++) {
-            for (int j = 0; j < i; j ++) {
-            }
-        }
+    static int solve(long n, int i) {
+        int[] array = new int[] {3,5,7};
     }
 }
