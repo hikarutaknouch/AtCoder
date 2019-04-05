07c955f7ac269509860a0778a799a65bfd63cccc
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-04-05 13:05:36 +0900

    yeah

--- ABC/088/c.cs
+++ ABC/088/c.cs
@@ -8,9 +8,18 @@ class Program
         c[0] = Console.ReadLine().Split().Select(int.Parse).ToArray();
         c[1] = Console.ReadLine().Split().Select(int.Parse).ToArray();
         c[2] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(solve(c) ? "Yes" : "No");
     }
-    static int solve(int[] c) {
+    static bool solve(int[][] c) {
         for (int i = 0; i < c.Length; i ++) {
+            int min = c[i].Min();
+            for (int j = 0; j < c[i].Length; j ++) {
+                c[i][j] -= min;
+            }
         }
+        for (int i = 0; i < c.Length; i ++) {
+            if(c[0][i] != c[1][i] || c[1][i] != c[2][i]) return false;
+        }
+        return true;
     }
 }
