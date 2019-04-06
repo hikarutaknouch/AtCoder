a18bae51b323ba76820d1bd64c565269f3b61aaf
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-04-06 13:24:38 +0900

    abc093

---
+++ ABC/093/a.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Contains('a') && s.Contains('b') && s.Contains('c') ? "Yes" : "No");
+    }
+}
