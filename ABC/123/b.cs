351ea83b5762c12a70903e34461ae4a0b1140ac7
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-04-07 10:46:53 +0900

    abc123

--- ABC/123/b.cs
+++ ABC/123/b.cs
@@ -5,11 +5,9 @@ class Program
 {
     static void Main(string[] args) {
         int[] l = new int[5];
-        l[0] = int.Parse(Console.ReadLine());
-        l[1] = int.Parse(Console.ReadLine());
-        l[2] = int.Parse(Console.ReadLine());
-        l[3] = int.Parse(Console.ReadLine());
-        l[4] = int.Parse(Console.ReadLine());
+        for (int i = 0; i < 5; i ++) {
+            l[i] = int.Parse(Console.ReadLine());
+        }
         int[] t = new int[5];
         for(int i = 0; i < 5; i ++) {
             t[i] = l[i];
