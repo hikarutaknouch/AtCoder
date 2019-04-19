d6a4af6c9a8485e9a45ce2c4f58eeb8d50b4ff84
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-04-19 20:03:22 +0900

    abc100-b

--- ABC/100/b.cs
+++ ABC/100/b.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Linq;
-
+ 
 class Program
 {
     static void Main(string[] args) {
         int[] s = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        double ans = Math.Pow(100, s[0]);
-        Console.WriteLine(ans * s[1]);
+        Console.WriteLine(solve(s[0], s[1]));
+    }
+    static int solve(int d, int n) {
+        var ans = 0;
+        if(n==100) ans = 101 * Math.Pow(100, d);
+        else ans = n * Math.Pow(100,d);
+        return ans;
     }
 }
