f448ae5ecbc27a1882bd10a0d15b120aa49d0ba3
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-04-29 18:05:19 +0900

    C++の練習

---
+++ APG4b/1.04.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+
+  cout << seconds << endl;
+  cout << seconds * 2 << endl;
+  cout << seconds * 5 << endl;
+  cout << seconds * 10 << endl;
+}
