63cdfa54657ae47272c6f26f483f3ffb8a6b3b76
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-05-03 01:39:57 +0900

    C++

---
+++ ABC/075/a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c; cin >> a >> b >> c;
+    if(a==b) cout << c << endl;
+    if(b==c) cout << a << endl;
+    if(c==a) cout << b << endl;
+}
