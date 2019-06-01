a0ff95c1f046ebf6ae08cbf7ad956f878923821d
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-06-01 14:54:13 +0900

    abc101-c

--- ABC/101/c.cs
+++ ABC/101/c.cs
@@ -1,4 +1,3 @@
-//未完
 using System;
 using System.Linq;
 
@@ -7,20 +6,10 @@ class Program
     static void Main(string[] args) {
         int[] s = Console.ReadLine().Split().Select(int.Parse).ToArray();
         int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        Console.WriteLine(solve(s[0], s[1], a));
+        Console.WriteLine(solve(s[0], s[1]));
     }
-    static int solve(int n, int k, int[] a) {
-        int i = Array.IndexOf(a, 1);
-        int ans = 0;
-        if(Math.Min(i+1, n-i) < k) {
-            ans = (n-1)/(k-1);
-            if((n-1)%(k-1) != 0) ans ++;
-        }
-        else {
-            ans = (n-1)/(k-1);
-            if((n-i)%(k-1) != 0) ans ++;
-            if(i%(k-1) != 0) ans ++;
-        }
+    static int solve(int n, int k) {
+        int ans = (n-1+k-2)/(k-1);
         return ans;
     }
 }
