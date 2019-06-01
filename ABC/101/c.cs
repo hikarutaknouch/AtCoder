aaf335a5f196a16c94ddcd13bdad6ba85eb7cb29
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-06-01 15:18:50 +0900

    add comment

--- ABC/101/c.cs
+++ ABC/101/c.cs
@@ -8,8 +8,10 @@ class Program
         int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
         Console.WriteLine(solve(s[0], s[1]));
     }
+    //k-1ずつ区間を変更する必要があるので、ans(k-1)+1>=nでなければならない。
+    //ans = (n-1)/(k-1)であり、こぼれる区間を拾うため切り上げる。a/bの切り上げは(a-1)/b + 1
     static int solve(int n, int k) {
-        int ans = (n-1+k-2)/(k-1);
+        int ans = (n-2+k-1)/(k-1);
         return ans;
     }
 }
