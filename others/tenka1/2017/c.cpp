f7f15f3ef71722cfe487259920c68f2cb6e4b165
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-06-03 11:12:13 +0900

    tenka1-2017-c

--- others/tenka1/2017/c.cpp
+++ others/tenka1/2017/c.cpp
@@ -1,9 +1,21 @@
-//not accepted
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+const long long MAX = 3500LL;
+
 int main() {
-    long N; cin >> N;
-    vector<long> ans(3, N);
-    if(N%2==0) ans[2] = N/2;
+    ll N; cin >> N;
+    for(ll h = 1; h <= MAX; h ++) {
+        for(ll n = 1; n <= MAX; n ++) {
+            ll x = N*h*n;
+            ll y = 4*h*n - N*(n+h);
+            if(y>0 && x%y==0) {//if(x%y==0 && y>0)だったらランタイムエラーになったのなんでだろ。
+                ll w = x/y;
+                cout << h << " " << n << " " << w << endl;
+                return 0;
+            }
+        }
+    }
+    return 0;
 }
