99b25666321f4b26ebee36a723011d51f245e3ef
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-06-22 17:00:41 +0900

    abc110-c

--- ABC/110/c.cpp
+++ ABC/110/c.cpp
@@ -1,24 +1,37 @@
-//未完
 #include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPP(i, n) for(int i = 1; i <= n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
 using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> p;
+const ll MOD = 1000000007;
 
 int main() {
     string s,t; cin >> s >> t;
-    vector<vector<int>> c(26, vector<int>(26, 0));
-    for(int i = 0; i < s.length(); i ++) {
-        c[s[i]-97][t[i]-97] = 2;
-        if(s[i]!=t[i]) {
-            if(c[s[i]-97][s[i]-97]>0) c[s[i]-97][t[i]-97] = 1;
+    int n = s.length();
+    vector<int> cs(26,-1);
+    vector<int> ct(26,-1);
+    bool ans = true;
+    REP(i,n) {
+        if(cs[s[i]-97]<0) cs[s[i]-97] = t[i]-97;
+        else {
+            if(cs[s[i]-97] != t[i]-97) {
+                ans = false;
+                break;
+            }
+        }
+        if(ct[t[i]-97]<0) ct[t[i]-97] = s[i]-97;
+        else {
+            if(ct[t[i]-97] != s[i]-97) {
+                ans = false;
+                break;
+            }
         }
     }
-    int count = 0;
-    for(int i = 0; i < 26; i ++) {
-        for(int j = 0; j < 26; j ++) {
-            if(c[i][j]==c[j][i]) continue;
-            if(c[i][j]!=c[j][i]) count += c[i][j];
-        }
-    }
-    cout << count << endl;
-    cout << (count%2==0 ? "Yes" : "No") << endl;
+    cout << (ans ? "Yes" : "No") << endl;
     return 0;
 }
