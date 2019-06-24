7650e6e47de3f9f183cfe06b7bbf848efd615a76
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-06-24 23:57:07 +0900

    abc078-c

---
+++ ABC/078/c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPP(i, n) for(int i = 1; i <= n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> p;
+const ll MOD = 1000000007;
+
+int main() {
+    int n, m; cin >> n >> m;
+    int x = (n-m)*100 + 1900*m;
+    int p = pow(2,m);
+    int ans = x*p;
+    cout << ans << endl;
+    return 0;
+}
