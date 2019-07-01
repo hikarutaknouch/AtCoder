7480457df2b76b7880c14828ec7900ea86804f40
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-07-01 16:25:15 +0900

    abc132

---
+++ ABC/132/c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPP(i, n) for(int i = 1; i <= n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> p;
+const ll MOD = 1000000007;
+
+int main() {
+    int n; cin >> n;
+    vector<int> d(n);
+    REP(i,n) cin >> d[i];
+    sort(ALL(d));
+    int ans = d[n/2] - d[n/2-1];
+    cout << ans << endl;
+    return 0;
+}
