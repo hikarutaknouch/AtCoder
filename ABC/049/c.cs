e022bd3455b12f5436bbf35c44f3efd6530bc000
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-07-03 13:40:15 +0900

    abc049-c

--- ABC/049/c.cs
+++ ABC/049/c.cs
@@ -1,59 +1,38 @@
 using System;
 using System.Linq;
-using System.Text;
 
 class Program
 {
-  static void Main(string[] args)
-  {
-    string s = Console.ReadLine();
-    string[] ss = {"dream","erace","dreamer","eracer"};
-    while(true) {
-      if(s.Length < 5) {
-        if (string.IsNullOrEmpty(s)) Console.WriteLine("YES");
-        else Console.WriteLine("NO");
-        break;
+  static void Main(string[] args) {
+      string s = Console.ReadLine();
+      Console.WriteLine(solve(s) ? "YES" : "NO");
+  }
+  static bool solve(string s) {
+    int n = s.Length;
+    char[] a = s.ToCharArray();
+    Array.Reverse(a);
+    s = new String(a);
+    bool ans = true;
+    string tmp;
+    for(int i = 0;i < n; i ++) {
+      tmp = s.Substring(i,5);
+      if(tmp == "esare" || tmp == "maerd") {
+        i += 4;
+        continue;
       }
-      else if (s.Length >= 5 && s.Length < 12) {
-        string s1 = s.Substring(s.Length - 5, 5);
-        if (s1 == ss[0]) {
-          s.Remove(s.Length - 5, 5);
-        }
-        else if (s1 == ss[1]) {
-          s.Remove(s.Length - 5, 5);
-        }
-        else {
-          string s2 = s.Substring(s.Length - 7, 7);
-          if (s2 == ss[2]) {
-            s.Remove(s.Length - 7, 7);
-          }
-          else if (s2 == ss[3]) {
-            s.Remove(s.Length - 7, 7);
-          }
-          else {
-            Console.WriteLine("NO");
-            break;
-          }
-        }
+      tmp = s.Substring(i,6);
+      if(tmp == "resare") {
+        i += 5;
+        continue;
       }
-      else {
-        string s1 = s.Substring(s.Length - 5, 5);
-        if (s1 == ss[0]) {
-          s.Remove(s.Length - 5, 5);
-        }
-        else if (s1 == ss[1]) {
-          s.Remove(s.Length - 5, 5);
-        }
-        else {
-          string s2 = s.Substring(s.Length - 7, 7);
-          if (s2 == ss[2]) {
-            s.Remove(s.Length - 7, 7);
-          }
-          if (s2 == ss[3]) {
-            s.Remove(s.Length - 7, 7);
-          }
-        }
+      tmp = s.Substring(i,7);
+      if(tmp == "remaerd") {
+        i += 6;
+        continue;
       }
+      ans = false;
+      break;
     }
+    return ans;
   }
 }
