e1bf8c2d4ae141b794b22a507a44fda619024f01
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-07-03 21:24:06 +0900

    agc005-a

---
+++ AGC/005/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> p;
+const ll MOD = 1000000007;
+
+int main() {
+    ifstream in("input.txt");
+    cin.rdbuf(in.rdbuf());
+    string x; cin >> x;
+    int n = x.length();
+    stack<char> st;
+    REP(i,n) {
+        if(x[i]=='S') st.push(x[i]);
+        else {
+            if(!st.empty() && st.top()=='S') st.pop();
+            else st.push(x[i]);
+        }
+    }
+    cout << st.size() << endl;
+    return 0;
+}
