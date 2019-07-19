5d87f5c72fee796620165d9c27af98f6aadbdc1b
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-07-19 13:50:45 +0900

    cf-2018

---
+++ others/CF/2018/b/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> p;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+int main() {
+    ifstream in("input.txt");
+    cin.rdbuf(in.rdbuf());
+    int n; cin >> n;
+    int ans = 100 - (100/n);
+    cout << ans << endl;
+    return 0;
+}
