a29c256a87a378d14a5cbaeecf78e69fc819f301
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-07-30 18:59:35 +0900

    kupc-2018-a

---
+++ others/kupc/2018/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+int main() {
+    ifstream in();
+    cin.rdbuf(in.rdbuf());
+    int n; cin >> n;
+    vector<int> s(n);
+    REP(i,n) cin >> s[i];
+    int ans = 0;
+    REP(i,n) {
+        int tmp; cin >> tmp;
+        ans = max(ans,tmp*s[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
