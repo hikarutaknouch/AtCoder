1bd82c65eb5540e8ede7ba2d8ad4bb4653023725
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-04 13:05:54 +0900

    cpsco2019-03-b

---
+++ others/cpsco/03/b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+int main() {
+    int n,m; cin >> n >> m;
+    vector<int> a(n);
+    REP(i,n) cin >> a[i];
+    sort(ALL(a), greater<int>());
+    int ans = 0;
+    int now = 0;
+    REP(i,n) {
+        now += a[i];
+        ans ++;
+        if(now>=m) break;
+    }
+    cout << ans << endl;
+    return 0;
+}
