dd4526b127f7bfc6006d0ce6c3659a6780d93e11
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-06 21:45:22 +0900

    tenka1-2016-qual-a

---
+++ others/tenka1/2016/a/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+int main() {
+    int ans = 0;
+    REPP(i,100) {
+        if(i%3==0 || i%5==0) continue;
+        else ans += i;
+    }
+    cout << ans << endl;
+    return 0;
+}
