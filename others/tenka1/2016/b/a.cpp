a4ca7b1caa1397ea78641b6d0279c69390fe39b8
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-08 23:24:17 +0900

    tenka1-2016

---
+++ others/tenka1/2016/b/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+ll f(double n) {
+    ll res = floor((pow(n,2)+4.0)/8.0);
+    return res;
+}
+
+int main() {
+    double n = 20;
+    ll ans = f(f(f(n)));
+    cout << ans << endl;
+    return 0;
+}
