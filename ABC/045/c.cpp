ee382e339836e132498464502e861a46edec5c09
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-21 15:19:47 +0900

    abc054

---
+++ ABC/045/c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    ifstream in("in.txt");
+    cin.rdbuf(in.rdbuf());
+    string s; cin >> s;
+    int n = s.length();
+    ll ans = 0;
+    REP(bit, (1<<n-1)) {
+        ll sum = 0;
+        string str = "";
+        str += s[0];
+        REP(i,n-1) {
+            if(bit & (1<<i)) {
+                sum += stoll(str);
+                str = "";
+                str += s[i+1];
+            } 
+            else str += s[i+1];
+        }
+        sum += stoll(str);
+        ans += sum;
+    }
+    cout << ans << endl;
+    return 0;
+}
