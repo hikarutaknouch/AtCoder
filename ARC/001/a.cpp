401b2cf3332e4f6f28155f95c6eeed9b1392d974
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-23 20:23:37 +0900

    arc001

---
+++ ARC/001/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<int> c(4);
+    REP(i,n) {
+        char chr; cin >> chr;
+        ++ c[chr-49];
+    }
+    cout << *max_element(ALL(c)) << " " << *min_element(ALL(c)) << endl;
+    return 0;
+}
