4c6e8ac1a04ec6c3db68fefaa0fcb972910f5f20
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-26 14:48:38 +0900

    agc012

---
+++ AGC/012/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    ll n; cin >> n;
+    vector<ll> a(3*n);
+    REP(i,3*n) cin >> a[i];
+    sort(a.rbegin(),a.rend());
+    ll ans = 0;
+    for(int i = 1; i < 3*n - (n-1); i += 2) ans += a[i];
+    cout << ans << endl;
+    return 0;
+}
