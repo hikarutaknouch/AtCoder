689c7e946e14433b0b1ca1c069cd47d1c3896aba
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-26 15:17:21 +0900

    abc072

---
+++ ABC/072/d.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<int> p(n), v;
+    REP(i,n) {
+        cin >> p[i];
+        if(i+1==p[i]) {
+            v.push_back(i);
+        }
+    }
+    int l = v.size();
+    ll ans = 0;
+    REP(i,l) {
+        if(i<l-1 && v[i+1]-v[i]==1) {
+            ++ ans;
+            ++ i;
+        }
+        else ++ans;
+    }
+    cout << ans << endl;
+    return 0;
+}
