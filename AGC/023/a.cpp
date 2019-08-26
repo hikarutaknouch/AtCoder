b4be22fe0dbadec4ffe9293943393b8a1252cc02
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-26 14:11:30 +0900

    agc023

---
+++ AGC/023/a.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<ll> a(n), s(n+1);
+    s[0] = 0;
+    REP(i,n) {
+        cin >> a[i];
+        s[i+1]=s[i]+a[i];
+    }
+    ll ans = 0;
+    sort(ALL(s));
+    ll cnt = 0;
+    REP(i,n) {
+        if(s[i]==s[i+1]) {
+            ++cnt;
+            ans += cnt;
+        }
+        else cnt = 0;
+    }
+    cout << ans << endl;
+    return 0;
+}
