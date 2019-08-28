73572b0cbeb80a4ec7f5bf59b40ab80c25109610
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-28 17:53:50 +0900

    ア

---
+++ others/nikkei/2019/exh/e.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    REPP(i,n) {
+        string ans = "";
+        if(i%2==0) ans += 'a';
+        if(i%3==0) ans += 'b';
+        if(i%4==0) ans += 'c';
+        if(i%5==0) ans += 'd';
+        if(i%6==0) ans += 'e';
+        if(ans=="") ans += to_string(i);
+        cout << ans << endl;
+    }
+    return 0;
+}
