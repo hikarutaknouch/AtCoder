b90103a823b9d587a151fd08f6e8672b8fbba246
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-31 10:55:44 +0900

    ア

---
+++ others/ttpc/2015/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n,b,c; cin >> n >> b >> c;
+    vector<int> a(n);
+    REP(i,n) cin >> a[i];
+    int ans = 0;
+    REPR(i,n) {
+        if(c==0) break;
+        ans += a[i]*min(b,c);
+        c -= min(c,b);
+    }
+    cout << ans << endl;
+    return 0;
+}
