8802056b876b176d2f02fe5163009f9b181e41b4
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-09-09 14:36:28 +0900

    ア

---
+++ ABC/140/c.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<int> b(n-1);
+    REP(i,n-1) cin >> b[i];
+    int ans = b[0]+b[n-2];
+    REP(i,n-2) ans += min(b[i],b[i+1]);
+    cout << ans << endl;
+    return 0;
+}
