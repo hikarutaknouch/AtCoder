c78244c0e2c94f5727e3f33a125de4d3737b2f99
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-09-10 20:41:50 +0900

    ア

---
+++ ABC/085/d.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+const ll MOD = 1000000007;
+const ll INF = 1000000000;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n,h; cin >> n >> h;
+    vector<int> a(n), b(n);
+    int amax = 0;
+    REP(i,n) {
+        cin >> a[i] >> b[i];
+        chmax(amax,a[i]);
+    }
+    sort(ALL(b),greater<int>());
+    ll ans = 0, idx = 0;
+    while(h>0 && b[idx]>=amax) {
+        h -= b[idx];
+        ++ ans;
+        ++ idx;
+    }
+    if(h>0) ans += h/amax + (h%amax ? 1 : 0);
+    cout << ans << endl;
+    return 0;
+}
