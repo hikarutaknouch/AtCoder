5e5e0888a24a957df9bc3040f190d4d72b9fceac
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-10-17 11:15:32 +0900

    MiKiNA

--- AGC/034/a.cpp
+++ AGC/034/a.cpp
@@ -1,25 +1,40 @@
-//not accepted
-//この問題はまだ早かった
 #include <bits/stdc++.h>
-#define REP(i, n) for(int i = 0; i < n; i++)
-
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
 using namespace std;
-
 typedef long long ll;
-typedef pair<ll, ll> p;
+const int MOD = 1000000007;
 
-const ll MOD = 10e9+7;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
 
 int main() {
-    int n,a,b,c,d; cin >> n >> a >> b >> c >> d;
-    a --; b--; c--; d--;
-    string s; cin >> s;
-    bool ans = 1;
-    REP(a,max(c,d)) {
-        if(s[i]=='#' && s[i+1]=='#') {
-            ans = 0;
-            break;
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n,a,b,c,d; cin >> n >> a >> b >> c >> d; --a,--b,--c,--d;
+    string s; cin >> s; s+= '#';
+    auto judge = [&](int x, int y) {
+        FOR(i,x,y) {
+            if(s[i]=='#'  && s[i+1]=='#') {
+                return false;
+            }
         }
+        return true;
+    };
+    auto btod = [&](int x, int y) {
+        FOR(i,x,y+1) {
+            if(s[i-1]=='.' && s[i]=='.' && s[i+1]=='.') {
+                return true;
+            }
+        }
+        return false;
+    };
+    bool ans = false;
+    if(judge(a,c) && judge(b,d)) {
+        if(c<d || btod(b,d)) ans = true;
     }
     cout << (ans ? "Yes" : "No") << endl;
     return 0;
