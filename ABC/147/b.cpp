d2c7fd0895b7649c42b5a3f787745be3b24f595c
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-12-14 16:09:25 +0900

    MiKiNA

---
+++ ABC/147/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPP(i, n) for(int i = 1; i <= n; ++i)
+#define REPR(i, n) for(int i = n-1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+const int MOD = 1000000007;
+
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b) {if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b) {if(a<b) a=b;}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    string s; cin >> s;
+    int n = s.length();
+    int ans = 0;
+    REP(i,n/2) {
+        if(s[i]!=s[n-1-i]) ++ ans;
+    }
+    cout << ans << endl;
+    return 0;
+}
